f0f5c2a3266444e95fdf1d960984c6c8951cd915
soundarya <soundarya.k@falconbrick.com>
2021-08-05 18:35:43 +0530

    Added button and function related to handling click

--- AwesomeApp/AwesomeApp/MainPage.xaml.cs
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -14,5 +14,11 @@ namespace AwesomeApp
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You Clicked {count} times";
+        }
     }
 }
